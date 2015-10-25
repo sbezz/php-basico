a9fab3949b9e0c033b4dbe8e6dd93b4a0511d84c
sbezz <bezz.sergio@gmail.com>
2015-10-25 12:51:19 -0200

    Renomeando o arquivo index.php para 2_trabalhando_com_variaveis

---
+++ 2_trabalhando_com_variaveis.php
@@ -0,0 +1,15 @@
+<?php
+
+    #Numero inteiro
+    $x = 1; //declarando a variavel x armazenando o valor '1'
+
+    #String
+    $nome = "School of Net"; //declarando uma String e armazenando o texto
+
+    // Exibindo o conteudo das variaveis
+    echo  $x;
+    echo $nome;
+
+
+?>
+
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $x = 1; //declarando a variavel x armazenando o valor '1'
-
-
-
-
-?>
-
