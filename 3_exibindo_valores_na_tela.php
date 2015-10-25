c649e29b8940cab68c0979a5d27ee4ad66f8453e
sbezz <bezz.sergio@gmail.com>
2015-10-25 13:21:45 -0200

    - Adicionando os novos arquivos.

---
+++ 3_exibindo_valores_na_tela.php
@@ -0,0 +1,11 @@
+
+<?php
+
+    $nome = "School of Net";
+
+    # echo $nome;     // usando o comando para simplesmente exibir o conteúdo
+    # print $nome;    // usando a função para imprimir na tela
+?>
+
+<h1> Seja Bem-Vindo <?php echo $nome ?> </h1>
+<p> A<?=$nome ?> Agradece a sua visita.</p>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+# operadores
+
+    $x = 10;
+    $y = 5;
+
+    $soma  = $x + $y;
+    $subtrai    = $x - $y;
+    $multiplica = $x * $y;
+    $divide     = $x / $y;
+
+    # echo $soma;
+    # echo $subtrai;
+    # echo $multiplica;
+    # echo $divide;
+#-------------------------------------------
+    # soma
+    $x++;  # $x = $x + 1;
+    $x+=5; # $x = $x + 5; usado para adicionar o 5 no valor de x
+
+    #subtração
+    $x--;  # $x = $x 1 1;
+    $x-=2; # $x = $x - 2; usado para subtrair o 2 no valor de x
+
+    # echo $x;
+#-------------------------------------------
+
+    # Concatenação
+    $primeiro_nome ="School";
+    $ultimo_nome = " of Net";
+
+    echo $primeiro_nome.$ultimo_nome;
+
+
+?>
